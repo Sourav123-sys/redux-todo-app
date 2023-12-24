69e3b23c5a71fc8f7fbc5d5c001c895859f7995e
Sourav123-sys <khansourav58@gmail.com>
2023-12-24 16:09:42 +0600

    project-setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div>
+      <h1 className="text-green-700 text-center text-3xl bg-black p-2 rounded"> React Redux Todo App</h1>
+    </div>
+  )
+}
+
+export default App
