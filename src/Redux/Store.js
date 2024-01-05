96791ad4a837a4390cf57359ce3581c6cd4f2a9e
Sourav123-sys <khansourav58@gmail.com>
2024-01-05 21:12:49 +0600

    thunk issue solve

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -1,7 +1,8 @@
 import { applyMiddleware, createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from "./RootReducer";
-import thunkMiddleware from "redux-thunk";
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunkMiddleware)));
+import   { thunk } from "redux-thunk";
+ "redux-thunk";
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 export default store;
